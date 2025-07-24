a7e1760ee9bf18cb60274a6a0db1f8b75d0d8e1e
Shashank <shashanksingh14370@gmail.com>
2025-07-24 18:50:19 +0530

    added blog titles and generate image page

--- client/src/pages/WriteArticle.jsx
+++ client/src/pages/WriteArticle.jsx
@@ -17,14 +17,16 @@ const WriteArticle = () => {
   }
 
   return (
-    <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-state-700'>
+    <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
       {/* Left Col */}
-      <form onSubmit={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-lg border border--gray-200'>
-        <div className="flex items-center gap-3">
+      <form onSubmit={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'>
+
+        <div className="flex items-center gap-3 mb-5">
           <Sparkles className='w-6 text-[#4A7AFF]'/>
           <h1 className='text-xl font-semibold'>Article Configuration</h1>
         </div>
-        <p>Article Topic</p>
+
+        <p className='wt-6 text-sm font-medium'>Article Topic</p>
         <input onChange={(e)=>setInput(e.target.value)}
         value={input}
         type="text" className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300' placeholder='The future of Artificial Intelligence is..' required/>
@@ -45,16 +47,17 @@ const WriteArticle = () => {
       </form>
       {/* Right Col */}
       <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px]'>
+
         <div className='flex items-center gap-3'>
           <Edit className='w-5 h-5 text-[#4A7AFF]'/>
-          <h1 className='text-xl font-semibold'>Gnerated Article</h1>
-          <div className='flex-1 flex justify-center items-center'>
+          <h1 className='text-xl font-semibold'>Generated Article</h1>
+        </div>
+        <div className='flex-1 flex justify-center items-center'>
             <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
-              <Edit className='w-9 h09 text-[#4A7AFF]'/>
+              <Edit className='w-9 h-9 '/>
               <p>Enter a topic and click "Generate article" to get started</p>
             </div>
           </div>
-        </div>
       </div>
     </div>
   )
